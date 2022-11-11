557d938395d28c2c4c9b54e011ec65db8a4f94dc
yukisk56 <yukisk56@gmail.com>
2022-11-11 15:45:42 +0000

    Add react-form-hook

--- src/pages/Create/Create.js
+++ src/pages/Create/Create.js
@@ -1,16 +1,32 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
 import Header from "../../components/Header/Header";
 import Link from "../../components/Link/Link";
+import DateInputGroup from "../../components/Form/DateInputGroup/DateInputGroup";
+import InputGroup from "../../components/Form/InputGroup/InputGroup";
 import "./Create.css";
 
 const Create = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = async data => { console.log(data); };
+
     return (
         <div className="create">
             <Header title="New Rumour" subtitle="Enter the details of the rumour you heard" />
-            <div className="create__links">
-                <Link label="Discard" to="/" iconBefore="chevronLeft" variant="secondary" />
-                <Link label="Validate and preview" to="/" variant="primary" />
-            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <DateInputGroup value="When did you hear it?" placeholder="" />
+                <InputGroup key="company-name" label="What company was it about?" placeholder="Company Name" name="company-name" value="" register={register} />
+                <InputGroup key="million" label="How many millions?" placeholder="999.999" value="" showTag name="million" register={register}/>
+                <InputGroup key="currency-code" label="In what currency?" placeholder="Currency code" name="currency-code" value="" showTag register={register}/>
+                <InputGroup key="codename" label="Source codename" placeholder="codename" name="currency-code" value="" showTag register={register}/>
+
+                <div className="create__links">
+                    <Link label="Discard" to="/" iconBefore="chevronLeft" variant="secondary" />
+                    <Link label="Validate and preview" to="/" variant="primary" />
+                </div>
+            </form>
         </div>
     )
 }
